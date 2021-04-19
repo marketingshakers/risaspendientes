bea0a43c4b0eb8a671f656fee3872835b5a7b917
Juan Villacorta <juanvillacortac@gmail.com>
2021-04-19 13:31:18 -0400

    Fixes

--- www/pages/nosotros/frase.tsx
+++ www/pages/nosotros/frase.tsx
@@ -4,7 +4,7 @@ export interface FraseProps {
 
 const Frase = ({ frase }: FraseProps) => (
   <div className="bg-x-blue-500 w-full py-24">
-    <div className="font-light mx-auto text-white text-center text-2xl italic sm:text-4xl lg:text-5xl" style={{width: '30ch'}}>
+    <div className="font-light mx-auto text-white text-center text-xl italic sm:text-4xl lg:text-5xl" style={{width: '30ch'}}>
       <p>{frase}</p>
     </div>
   </div>
